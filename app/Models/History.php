b9c25f7726965c0650b614d19e9e1903b4397894
Tanabe <12tanasuke06@gmail.com>
2021-08-04 23:36:37 +0900

    Laravel_relation_1

--- app/Models/History.php
+++ app/Models/History.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use APP\Models\User;
 
 class History extends Model
 {
     use HasFactory;
     protected $table = 'history';
     protected $primaryKey = 'history_id';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class);
+    }
 }
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,10 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use APP\Models\Review;
+use APP\Models\History;
 
 class Movie extends Model
 {
     use HasFactory;
     protected $table = 'movie';
     protected $primaryKey = 'movie_id';
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function histories()
+    {
+        return $this->hasMany(History::class);
+    }
+
 }
