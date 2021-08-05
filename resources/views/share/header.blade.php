9ba6521f7d34b8ad659fb6526d1bd95af2313f7d
p0x0q <info.allegation@gmail.com>
2021-08-05 14:23:55 +0900

    fix: header icon loc

--- resources/views/share/header.blade.php
+++ resources/views/share/header.blade.php
@@ -34,7 +34,7 @@
             <div>
                 {{Auth::user()->username}}
                 <a href="/mypage">
-                    <img src="./images/user_icon.png" width="30" height="30" alt="">
+                    <img src="/images/user_icon.png" width="30" height="30" alt="">
                 </a>
                 <button type="button" onclick="location.href='/logout'" class="btn btn-outline-secondary">LOGOUT</button>
             </div>
