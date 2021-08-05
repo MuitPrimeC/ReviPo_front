a99f0f33c8a027f73d7e096c57815d7693660be9
p0x0q <info.allegation@gmail.com>
2021-08-05 14:39:36 +0900

    make: search server-side

--- app/Http/Controllers/MovieSearchController.php
+++ app/Http/Controllers/MovieSearchController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
 class MovieSearchController extends Controller
 {
 
@@ -11,7 +14,25 @@ class MovieSearchController extends Controller
      */
     public function index()
     {
-        return view('movielist');
+        return view('movielist', ["movielist" => []]);
+    }
+
+    /**
+     * name store
+     * desc
+     *
+     * @queryParam title string Example: イベントタイトル
+     */
+    public function store(Request $request)
+    {
+        $valid_dict = [
+            'q' => ['required'],
+        ];
+
+        $request->validate($valid_dict);
+        $search_query = $request->q;
+        $limit = $request->has('limit') ? intval($request['limit']) : 10;
+        return response(Movie::whereLike("title", $search_query)->orWhereLike("description", $search_query)->simplePaginate($limit));
     }
 
 }
