05614b14521df99fd118515ad289592673387d6a
mizumura <mizumura.tus@gmail.com>
2021-08-05 22:20:15 +0900

    デザイン微調整

--- resources/views/point_exchange.blade.php
+++ resources/views/point_exchange.blade.php
@@ -18,6 +18,7 @@
         </div>
       </div>
 
+      <div class="container">
       <div class="card-deck">
             <div class="card text-center">
                 <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="./icon_112380_128.png" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Image cap</text></svg>
@@ -115,6 +116,8 @@
         }
       </style>
 
+    </div>
+    <br><br><br>
     </body>
 </html>
 
