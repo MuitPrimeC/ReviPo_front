ab49cd3d318c956c8e81cabedd66fde889d91580
p0x0q <info.allegation@gmail.com>
2021-08-05 14:32:19 +0900

    make: orWhere

---
+++ app/Models/Traits/WhereLike.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Models\Traits;
+
+trait WhereLike
+{
+    // 部分一致検索
+    public function scopeWhereLike($query, string $column, string $keyword)
+    {
+        return $query->where($column, 'like', '%' . addcslashes($keyword, '%_\\') . '%');
+    }
+    public function scopeorWhereLike($query, string $column, string $keyword)
+    {
+        return $query->orWhere($column, 'like', '%' . addcslashes($keyword, '%_\\') . '%');
+    }
+
+    // 前方一致検索
+    public function scopeWhereLikeForward($query, string $column, string $keyword)
+    {
+        return $query->where($column, 'like', addcslashes($keyword, '%_\\') . '%');
+    }
+}
