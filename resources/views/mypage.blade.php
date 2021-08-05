e116bbd230309b34078925a5c4119490dabddd2d
19rd063 <19rd063@ms.dendai.ac.jp>
2021-08-06 01:13:08 +0900

    mypage:レビュー/閲覧タブ追加　タブ内バックの動き加筆修正お願いします

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -4,7 +4,7 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-
+        <script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <link rel="stylesheet" href="test.css">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
@@ -43,41 +43,98 @@
             </div>
         </div>
     </diV>
-    <div class="container-fluid">
-        <br>
-        <h2 class="text-center">最近レビューした作品</h2>
-        <br>
-        <div class="row">
-            <div class="col-2">
-                <a href="https://p0x0q.com/"><img src="images/ad1.jpg" width=100></a>
-            </div>
-            <div class="col-10">
-                <div class="container-fluid">
-                    <div class="row">
-                        @foreach($recent_review_movies as $review)
-                        <div class="card md-auto col-6" style="max-width: 540px; max-height:200px;">
-                            <div class="row no-gutters">
-                                <div class="col-md-4">
-                                    <img src="/images/movie/{{$review->movie->filename}}" class="bd-placeholder-img card-img-top embed-responsive-item">
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title"><a href="/movie/{{$review->movie->movie_id}}">movie title:{{$review->movie->title}}</a></h5>
-                                        <p class="card-text">review title:{{$review->title}}</p>
-                                        <p class="card-text">feature:{{$review->feature}}</p>
-                                        <p class="card-text">score:{{$review->score}}</p>
-                                        <p class="card-text">description:{{$review->description}}</p>
-                                        <p class="card-text"><small class="text-muted">{{$review->updeted_at}}</small></p>
+
+        
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <a class="nav-link active" id="review-tab" data-toggle="tab" href="#review" role="tab" aria-controls="review" aria-selected="true">最近レビューした作品</a>
+        </li>
+        <li class="nav-item" role="presentation">
+            <a class="nav-link" id="view-tab" data-toggle="tab" href="#view" role="tab" aria-controls="view" aria-selected="false">最近閲覧した作品</a>
+        </li>
+    </ul>
+
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="review" role="tabpanel" aria-labelledby="review-tab">
+
+            <div class="container-fluid">
+                <br>
+                <h2 class="text-center">最近レビューした作品</h2>
+                <br>
+                <div class="row">
+                    <!--<div class="col-2">
+                        <a href="https://p0x0q.com/"><img src="images/ad1.jpg" width=100></a>
+                    </div>-->
+                    <div class="col-10">
+                        <div class="container-fluid">
+                            <div class="row">
+                                @foreach($recent_review_movies as $review)
+                                <div class="card md-auto col-6" style="max-width: 540px; max-height:200px;">
+                                    <div class="row no-gutters">
+                                        <div class="col-md-4">
+                                            <img src="/images/movie/{{$review->movie->filename}}" class="bd-placeholder-img card-img-top embed-responsive-item">
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title"><a href="/movie/{{$review->movie->movie_id}}">movie title:{{$review->movie->title}}</a></h5>
+                                                <p class="card-text">review title:{{$review->title}}</p>
+                                                <p class="card-text">feature:{{$review->feature}}</p>
+                                                <p class="card-text">score:{{$review->score}}</p>
+                                                <p class="card-text">description:{{$review->description}}</p>
+                                                <p class="card-text"><small class="text-muted">{{$review->updeted_at}}</small></p>
+                                            </div>
+                                        </div>
                                     </div>
+                                </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="tab-pane fade" id="view" role="tabpanel" aria-labelledby="view-tab">
+
+            <div class="container-fluid">
+                    <br>
+                    <h2 class="text-center">最近閲覧した作品</h2>
+                    <br>
+                    <div class="row">
+                        <!--<div class="col-2">
+                            <a href="https://p0x0q.com/"><img src="images/ad1.jpg" width=100></a>
+                        </div>-->
+                        <div class="col-10">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    @foreach($recent_review_movies as $review)
+                                    <div class="card md-auto col-6" style="max-width: 540px; max-height:200px;">
+                                        <div class="row no-gutters">
+                                            <div class="col-md-4">
+                                                <img src="/images/movie/{{$review->movie->filename}}" class="bd-placeholder-img card-img-top embed-responsive-item">
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h5 class="card-title"><a href="/movie/{{$review->movie->movie_id}}">movie title:{{$review->movie->title}}</a></h5>
+                                                    <p class="card-text">review title:{{$review->title}}</p>
+                                                    <p class="card-text">feature:{{$review->feature}}</p>
+                                                    <p class="card-text">score:{{$review->score}}</p>
+                                                    <p class="card-text">description:{{$review->description}}</p>
+                                                    <p class="card-text"><small class="text-muted">{{$review->updeted_at}}</small></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
-                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 <br><br>
 </body>
 @include('share.footer');
