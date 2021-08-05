f175f5ed777b20420c2a94835bb95ce27348dae7
p0x0q <info.allegation@gmail.com>
2021-08-05 16:25:24 +0900

    add api route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\api\GetUserController;
+use App\Http\Controllers\api\PostRecommendedController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MovieInfoController;
