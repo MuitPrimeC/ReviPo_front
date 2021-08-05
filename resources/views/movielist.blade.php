64cbdbbaa57a978a935dff06b19f219f46aee8a0
19rd063 <19rd063@ms.dendai.ac.jp>
2021-08-05 20:14:00 +0900

    左右マージンに余白を追加

--- resources/views/movielist.blade.php
+++ resources/views/movielist.blade.php
@@ -45,6 +45,8 @@
         <style>
             div.card-list{
             margin-top: 20px;
+            margin-left: 5px;
+            margin-right: 5px;　
             }
         </style>
         <div class="paginate d-flex justify-content-center">
