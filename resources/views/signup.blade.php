4d372f03184c2e910b362e47b5a0f6e1315659e3
p0x0q <info.allegation@gmail.com>
2021-08-05 13:18:49 +0900

    replace: signup

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -8,6 +8,7 @@
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </head>
+
   <body  class="text-center" >
     <a id="skippy" class="sr-only sr-only-focusable" href="#content">
       <div class="container">
@@ -15,21 +16,18 @@
       </div>
     </a>
 
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <font color="#0000ff">{{ $error }}</font><br>
-        @endforeach
-    @endif
     <form class="form-signin" method="post" action="?">
-      <img class="mb-4" src="./icon_112380_128.png" alt="" width="72" height="72">
-        <h1 class="h3 mb-3 font-weight-normal">Please sign up</h1>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" name="username" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox mb-3"></div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign Up</button>
-      </form>
+        <img class="mb-4" src="./icon_112380_128.png" alt="" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">Please sign up!</h1>
+        <label for="inputUsername" class="sr-only">username</label>
+        <input type="text" name="username" id="inputUsername" class="form-control" placeholder="username" required autofocus>
+        <label for="inputPassword" class="sr-only">password</label>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="password" required>
+        <label for="re-inputPassword" class="sr-only">Re-input password</label>
+        {{-- <input type="password" id="re-inputPassword" class="form-control" placeholder="Re-input password" required>
+        <div class="checkbox mb-3"></div> --}}
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up!</button>
+    </form>
       <style>
         .form-signin {
           width: 100%;
