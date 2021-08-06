1d78cf359be511abfa7db4aa38f2acdf4be7048f
19rd063 <19rd063@ms.dendai.ac.jp>
2021-08-06 12:48:03 +0900

    set wight

--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -14,7 +14,7 @@
         <div class="card mb-3">
             <div class="row no-gutters">
                 <div class="col-md-4">
-                    <img src="/images/movie/{{$movie->filename}}" alt="...">
+                    <img src="/images/movie/{{$movie->filename}}" wight="350" alt="...">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
