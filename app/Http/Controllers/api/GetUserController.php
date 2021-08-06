1c56a8b2fbf5069d50ef80246d24763451f536a3
p0x0q <info.allegation@gmail.com>
2021-08-06 10:31:33 +0900

    bugfix: api/user/all

--- app/Http/Controllers/api/GetUserController.php
+++ app/Http/Controllers/api/GetUserController.php
@@ -18,9 +18,11 @@ class GetUserController extends Controller
         foreach (User::all() as $user) {
             $movie_ids = [];
             foreach (History::where('user_id', $user['user_id'])->get() as $history) {
-                array_push($movie_ids, $history['movie_id']);
+                if (array_key_exists($history->movie_id, $movie_ids)) {
+                    continue;
+                }
+                array_push($movie_ids, $history->movie_id);
             }
-            $movie_ids = array_unique($movie_ids);
             array_push($r, ['user_id' => $user['user_id'], 'history' => $movie_ids]);
         }
         return $r;
