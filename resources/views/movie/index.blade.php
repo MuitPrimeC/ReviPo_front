e5d465f4944fa2ad608d42e924078ec9fa4933dd
19rd063 <19rd063@ms.dendai.ac.jp>
2021-08-06 10:49:50 +0900

    レビュー評価ボタンを追加、評価軸（暫定）を設定

--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -28,10 +28,10 @@
 
         <ul class="nav nav-tabs" id="myTab" role="tablist">
             <li class="nav-item" role="presentation">
-              <a class="nav-link active" id="read-tab" data-toggle="tab" href="#read" role="tab" aria-controls="read" aria-selected="true">Read</a>
+              <a class="nav-link active" id="read-tab" data-toggle="tab" href="#read" role="tab" aria-controls="read" aria-selected="true">レビュー閲覧</a>
             </li>
             <li class="nav-item" role="presentation">
-              <a class="nav-link" id="write-tab" data-toggle="tab" href="#write" role="tab" aria-controls="write" aria-selected="false">Write</a>
+              <a class="nav-link" id="write-tab" data-toggle="tab" href="#write" role="tab" aria-controls="write" aria-selected="false">レビュー投稿</a>
             </li>
         </ul>
 
@@ -50,13 +50,15 @@
                       <p class="card-text">feature:{{$review->feature}}</p>
                       <p class="card-text">score:{{$review->score}}</p>
                       <p>
-                        <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                          See more
-                        </a>
+                        <p><button type="submit" class="btn btn-primary">参考になった！</button></p>
+                        <!--<a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                          レビューの詳細を見る
+                        </a>-->
                         <form role="form" method="post" action="/review_score">
                         <input type="hidden" name="">
                         </form>
                       </p>
+
                       <div class="collapse" id="collapseExample">
                           description:{{$review->description}}
                       </div>
@@ -72,33 +74,38 @@
                 <form class="form-review" method="post" action="/movie">
                     <input type="hidden" name="movie_id" value="{{$movie_id}}">
                     <div class="form-group">
-                        <label for="exampleFormControlTextarea1">review title</label>
+                        <label for="exampleFormControlTextarea1">ひとことレビュー！</label>
                         <textarea name="title" class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
                     </div>
 
                     <div class="feature">
                         <div class="form-group">
-                        <label for="exampleFormControlSelect1">feature select</label>
+                        <label for="exampleFormControlSelect1">評価軸を選択</label>
                         <select name="feature" class="form-control" id="exampleFormControlSelect1">
-                            <option>good</option>
-                            <option>bad</option>
-                            <option>etc</option>
-                            <option>etc</option>
-                            <option>etc</option>
+                            <option>ストーリー</option>
+                            <option>構成</option>
+                            <option>音楽（劇伴・主題歌・挿入歌）</option>
+                            <option>映像</option>
+                            <option>演出</option>
+                            <option>演技</option>
+                            <option>キャスト（俳優・女優・声優）</option>
+                            <option>特によかったところ</option>
+                            <option>残念だったところ</option>
+                            <option>その他</option>
                         </select>
                     </div>
                     <div class="form-group">
-                        <label for="formControlRange">rating Range input</label>
+                        <label for="formControlRange">評価度</label>
                         <input type="range" name="score" class="form-control-range" min="0" max="5" id="formControlRange">
                     </div>
                     <div class="form-group">
-                        <label for="exampleFormControlTextarea1">review textarea</label>
+                        <label for="exampleFormControlTextarea1">レビュー詳細</label>
                         <textarea name="description" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div>
 
                     <div class="col-auto my-1">
-                        {{-- <button type="button" class="btn btn-info">Add other features</button> --}}
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <!-- <button type="button" class="btn btn-info">Add other features</button> -->
+                        <button type="submit" class="btn btn-primary">投稿</button>
                     </div>
 
                 </form>
