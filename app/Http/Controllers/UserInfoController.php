ce689da11e2cae1386680533a73fbf1bc60b39a5
p0x0q <info.allegation@gmail.com>
2021-08-06 11:02:48 +0900

    fix: unique movie_iods

--- app/Http/Controllers/UserInfoController.php
+++ app/Http/Controllers/UserInfoController.php
@@ -20,10 +20,21 @@ class UserInfoController extends Controller
             $review = $d->get();
         } else {
             $review = [];
-            $user_history = history::where('user_id', Auth::id())->orderBy('updated_at', 'desc')->distinct()->select('movie_id'); #->simplePaginate(10);
-            // dd($user_history);
-
-            return view('mypage', ['recent_review_movies' => $recent_review_movies, 'tickets' => $review, 'user_histories' => $user_history]);
         }
+        $user_histories = History::where('user_id', Auth::id());
+        $movie_ids = [];
+        foreach ($user_histories->get() as $u) {
+            if (!in_array($u->movie_id, $movie_ids)) {
+                array_push($movie_ids, $u->movie_id);
+            }
+        }
+        $user_history = [];
+        if ($movie_ids != []) {
+            $user_history = History::whereIn('movie_id', $movie_ids)->orderBy('updated_at', 'desc')->simplePaginate(10);
+        }
+        // dd($user_history);
+        // dd($movie_ids);
+
+        return view('mypage', ['recent_review_movies' => $recent_review_movies, 'tickets' => $review, 'user_histories' => $user_history]);
     }
 }
